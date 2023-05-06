d8b64be43370d8ca5980f64e9187c3cdd84b6e93
Riza Fahmi <rizafahmi@gmail.com>
2023-05-06 21:49:48 +0700

    Add sensus/absen page

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ Nama alternatif
 - komunita
 - sensus (sens.us)
 
+## Konsep
+
 [Konsep](https://www.tldraw.com/r/v2_c_WAG6ukuWcFCY5EMLrXqix?viewport=-692%2C-1018%2C1458%2C985&page=page%3AW-f57ZEdamYLEYLQB_J40)
--- server.js
+++ server.js
@@ -39,6 +39,16 @@ server.post('/jakartajs/events/new', function(req, res) {
   res.redirect('/jakartajs')
 })
 
+// Route untuk absensi
+// Halaman ada nama peserta yang autocomplete
+server.get('/jakartajs/events/detail/sensus', function(req, res) {
+  const event = { title: "#61 JakartaJS X Hijra" }
+  res.render('events/sensus.html', { community, event, title: event.title })
+})
+
+server.post('/jakartajs/events/detail/sensus', function(req, res) {
+  res.redirect('/jakartajs/events/detail')
+})
 
 server.get('/ping', function(req, res) {
   res.json({ status: 'OK' })
